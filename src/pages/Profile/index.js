6b0a95011ffaed9ae173fb0bbdc93831f86876e8
Gustavo Prizon <gustavoprizon@iMac-de-Gustavo.local>
2020-01-06 13:23:49 -0300

    criada a pagina do perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <hr />
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmar nova senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
